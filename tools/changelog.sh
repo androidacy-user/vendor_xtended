9f1295b5499d0bb81f2a436460d8d9f13a28a895
Jack Summers <jacksummers715@gmail.com>
2020-04-23 05:09:59 -0400

    changelog fix

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -17,15 +17,8 @@ export Until_Date=`date --date="$k days ago" +%F`
 echo "====================" >> $Changelog;
 echo "     $Until_Date    " >> $Changelog;
 echo "====================" >> $Changelog;
-# Cycle through every repo to find commits between 2 dates
-while read path;
-        do
-    CLOG=$(git --git-dir ./${path}/.git log --oneline --after=$After_Date --until=$Until_Date);
-    if [ -n "$CLOG" ]; then
-       echo project $path >> $Changelog
-       git --git-dir ./${path}/.git log --oneline --after=$After_Date --until=$Until_Date >> $Changelog
-    fi
-done < ./.repo/project.list;
+repo forall -c "GIT_LOG=\`git log --oneline --after=$After_Date --until=$Until_Date\` ; if [ ! -z \"\$GIT_LOG\" ]; then printf  '\n   * '; realpath \`pwd\` | sed 's|^$CURRENT_PATH/||' ; printf \"\$GIT_LOG\"; fi" >> $Changelog
+
 echo "" >> $Changelog;
 done
 
